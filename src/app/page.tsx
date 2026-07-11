237bf1e92fc5b272c211f7cd2312b8b3b95276f7
DaniloMelo <danilomarquesdemelo@gmail.com>
2026-07-11 19:28:48 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <h1>Portifólio</h1>
+      <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUFS5gRaxwvQL5yFqJZ5c8JA25gJ41FRvC-_oIt3Ft37QWtjX2NcIUVUz&s=10"
+        alt="meme"
+        width="500"
+        height="500"
+      />
     </div>
   );
 }
