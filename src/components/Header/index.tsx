79959e8948093fe9c2f1e11885ffcd685f80be38
DaniloMelo <danilomarquesdemelo@gmail.com>
2026-07-17 12:07:41 -0300

    fix: header component responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,19 @@
 import Link from "next/link";
 import Theme from "../Theme";
 import Container from "../Container";
+import { cn } from "@/utils/cn";
 
 export default function Header() {
   return (
-    <header className="">
+    <header
+      className={cn(
+        "w-full fixed top-0 z-50",
+        "bg-secondary-background dark:bg-secondary-background",
+        // "bg-red-500",
+      )}
+    >
       <Container className="px-0 sm:px-0 sm:py-0 lg:px-0 lg:py-0">
-        <div className="flex items-center justify-end gap-x-6 p-2">
+        <div className="flex items-center justify-center lg:justify-end gap-x-8 p-2">
           <nav className="flex gap-6">
             <Link href="/#hero">Início</Link>
 
@@ -25,7 +32,3 @@ export default function Header() {
     </header>
   );
 }
-
-/*
-  w-full fixed top-0 z-50 bg-secondary-background dark:bg-secondary-background
-*/
