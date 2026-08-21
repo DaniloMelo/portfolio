fd26f3c43ca5ebd864e6268f5575a8b0de53b22b
DaniloMelo <danilomarquesdemelo@gmail.com>
2026-08-21 18:08:39 -0300

    feat: 'addProject' service now handle last position creation

--- src/services/project/addProject.ts
+++ src/services/project/addProject.ts
@@ -1,11 +1,13 @@
 import { TechnologyNotFound } from "@/errors/project/TechnologyNotFound";
 import {
   createProject,
+  findLastProjectPosition,
   findTechnologies,
 } from "@/repository/project/projectRepository";
 import { Project } from "@/types/project";
+import { createSlug } from "@/utils/createSlug";
 
-export async function addProject(project: Project) {
+export async function addProject(project: Omit<Project, "position">) {
   const allTechs = await findTechnologies();
   const allTechsNames = allTechs.map((tech) => tech.name);
   project.technologies.map((tech) => {
@@ -14,5 +16,19 @@ export async function addProject(project: Project) {
     }
   });
 
-  await createProject(project);
+  const lastProjectPosition = await findLastProjectPosition();
+
+  const newProject: Project = {
+    title: project.title,
+    slug: createSlug(project.title),
+    description: project.description,
+    about: project.about,
+    repositoryCodeUrl: project.repositoryCodeUrl,
+    deployUrl: project.deployUrl,
+    position: lastProjectPosition + 1,
+    technologies: project.technologies,
+    images: project.images,
+  };
+
+  await createProject(newProject);
 }
